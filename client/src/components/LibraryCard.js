84d3c62ebc5fe05e05405d74eebaeb16c5840c59
Felix Canditt <felix.canditt@gmail.com>
2021-06-24 11:39:56 +0200

    added stars to library cards

--- client/src/components/LibraryCard.js
+++ client/src/components/LibraryCard.js
@@ -2,18 +2,44 @@ import styled from 'styled-components';
 
 import displayCategory from '../lib/displayCard';
 
+import starIcon from '../images/star.svg';
+
 export default function LibraryCard({
   item,
   onSetItemToBeEdited,
   onRemoveFromLibrary
 }) {
+  function displayStar(positionOfClickedStar) {
+    return (
+      <img
+        src={starIcon}
+        alt="Stern zeigt die Bewertung an"
+        style={
+          item.rating >= positionOfClickedStar
+            ? { opacity: '100%' }
+            : { opacity: '25%' }
+        }
+      />
+    );
+  }
+
   return (
     <Card>
       {item.title ? <p>{item.title}</p> : <p>Ohne Titel</p>}
 
       {displayCategory(item.category)}
 
-      {item.rating ? <p>Meine Bewertung: {item.rating}/5 Sterne</p> : ''}
+      {item.rating ? (
+        <Stars>
+          {displayStar(1)}
+          {displayStar(2)}
+          {displayStar(3)}
+          {displayStar(4)}
+          {displayStar(5)}
+        </Stars>
+      ) : (
+        ''
+      )}
 
       {item.notes ? (
         <p>
@@ -33,12 +59,12 @@ export default function LibraryCard({
 const Card = styled.article`
   box-shadow: 0.3rem 0.3rem 0.8rem lightgrey;
   border-radius: 0.4rem;
-  padding: 1.2rem 1rem;
+  padding: 2rem;
   background: hotpink;
   color: ivory;
 
   display: grid;
-  gap: 0.8rem;
+  gap: 2rem;
 
   button {
     justify-self: end;
@@ -48,5 +74,15 @@ const Card = styled.article`
     background: turquoise;
     padding: 0.2rem 0.3rem;
     font-size: 1.25rem;
+  }
+`;
+
+const Stars = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  img {
+    width: 2rem;
+    color: white;
   }
 `;
