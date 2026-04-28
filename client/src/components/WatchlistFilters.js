bfb2b8694a3042e90e495b5870362c7a735deac5
Felix Canditt <felix.canditt@gmail.com>
2026-04-28 13:25:28 +0200

    spaced filters

--- client/src/components/WatchlistFilters.js
+++ client/src/components/WatchlistFilters.js
@@ -9,28 +9,29 @@ export default function WatchlistFilters({ onSetFilter }) {
   return (
     <FilterContainer>
       <FilterBox>
-        <ul>
-          {categories.map((item) => (
-            <li>
-              <button onClick={() => filterList(item.name)}>{item.text}</button>
-            </li>
-          ))}
+        {categories.map((item) => (
           <li>
-            <button onClick={() => filterList('')}>X</button>
+            <button onClick={() => filterList(item.name)}>{item.text}</button>
           </li>
-        </ul>
+        ))}
+        <li>
+          <button onClick={() => filterList('')}>Filter entfernen</button>
+        </li>
       </FilterBox>
     </FilterContainer>
   );
 }
 
 const FilterContainer = styled.div`
+  margin-top: 3rem;
   display: flex;
   justify-content: center;
 `;
 
-const FilterBox = styled.div`
+const FilterBox = styled.ul`
   width: 85vw;
   max-width: 20rem;
-  border: 1px black solid;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
 `;
