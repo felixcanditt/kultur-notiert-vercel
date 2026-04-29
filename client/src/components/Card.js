df06165ccc009ad33edee94725972f573c38809a
Felix Canditt <felix.canditt@gmail.com>
2026-04-29 11:46:37 +0200

    created card component file

---
+++ client/src/components/Card.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default function Card({}) {
+  return <div>hallo</div>;
+}
+const CategoryContainer = styled.div`
+  margin-bottom: 0.8rem;
+  display: flex;
+`;
